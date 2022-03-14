e8be865594b4df3312bafc9e23bd3c34eeeb766a
MrYannick29 <yannick.saelen@gmail.com>
2022-03-14 11:02:34 +0100

    Create Uitschrijver class

--- BB_AI_project/src/pack/Auto.java
+++ BB_AI_project/src/pack/Auto.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Auto {
 	private String id;
 	private ArrayList<Request> allRequests;
+	private Zone zone;
 	
 	public Auto(String id, ArrayList<Request> allRequests) {
 		this.id=id;
@@ -23,6 +24,14 @@ public class Auto {
 	}
 	public void setAllRequests(ArrayList<Request> allRequests) {
 		this.allRequests = allRequests;
+	}
+
+	public Zone getZone() {
+		return zone;
+	}
+
+	public void setZone(Zone zone) {
+		this.zone = zone;
 	}
 	
 	
---
+++ BB_AI_project/src/pack/Uitschrijver.java
@@ -0,0 +1,57 @@
+package pack;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Uitschrijver {
+
+	private Oplossing opl;
+
+	public Uitschrijver(Oplossing opl) {
+		this.opl = opl;
+	}
+
+	public Oplossing getOpl() {
+		return opl;
+	}
+
+	public void setOpl(Oplossing opl) {
+		this.opl = opl;
+	}
+	
+	public void Schrijven() throws IOException {
+		FileWriter fw = new FileWriter("out.txt");
+		
+		//write Auto-to-Zone
+		fw.write("100");
+		fw.write("+Vehicle assignments");
+		ArrayList<Auto> autos = this.opl.getAutos();
+		for (int i = 0; i < autos.size(); i++) {
+			String s = autos.get(i).getId() +";"+ autos.get(i).getZone().getId();
+			fw.write(s);
+		}
+		//write gelukte requests
+		fw.write("+Assigned requests");
+		ArrayList<Request> req = this.opl.getReq();
+		for (int i = 0; i < req.size(); i++) {
+			if(req.get(i).getToegAuto()!=null)
+			{
+				String s = req.get(i).getID() +";"+ req.get(i).getToegAuto().getId();
+				fw.write(s);
+			}
+			
+		}
+		//write mislukte requests
+		fw.write("+Unassigned requests");
+		for (int i = 0; i < req.size(); i++) {
+			if(req.get(i).getToegAuto()==null)
+			{
+				String s = req.get(i).getID();
+				fw.write(s);
+			}
+		}
+	 
+		fw.close();
+	}
+}
