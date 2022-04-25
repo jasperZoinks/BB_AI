297bb0a831d2a6f42e7d9d9fda86e1c0565bc9e7
jasperZoinks <79266435+jasperZoinks@users.noreply.github.com>
2022-04-25 21:10:59 +0200

    extra voor de testen te runnen

--- BB_AI_project/src/pack/Main.java
+++ BB_AI_project/src/pack/Main.java
@@ -7,12 +7,19 @@ public class Main {
 	
 	public Main() {
 		Inlezer nInlezer=new Inlezer();
-		String pathToFile = "\\src\\210_5_44_25.csv";
+		String pathToFile = "\\src\\360_5_71_25.csv";
 		Oplossing firstSol=nInlezer.readIn(pathToFile);
 		//System.out.println(firstSol.toString());
 		InitOpl initOpl = new InitOpl(firstSol);
+		
 		initOpl.verdeelAutos();
-		this.writeSolution(initOpl.getOpl());
+		//initOpl.verdeelAutosRandom();
+		
+		long start = System.currentTimeMillis();
+		int seconds = 300;	//dit is 5 minuten
+		long end = start + (seconds * 1000) ;
+		
+		//this.writeSolution(initOpl.getOpl());
 		
 		//Optie 1
 		/*
@@ -21,40 +28,41 @@ public class Main {
 		Oplossing newOpl =bestOpl;		
 		
 		int counter=0;
-		while(true) {
+		while(System.currentTimeMillis()<end) {
 			counter++;
 			newOpl = bestOpl.makeChanges();
 						
 			if(newOpl.getKost()<bestOpl.getKost()) {
-				System.out.println("--------");
-				System.out.println("better solution found!");
-				System.out.println("Old Cost: "+ bestOpl.getKost()+" , New Cost : "+newOpl.getKost());
+				//System.out.println("--------");
+				//System.out.println("better solution found!");
+				//System.out.println("Old Cost: "+ bestOpl.getKost()+" , New Cost : "+newOpl.getKost());
 				
 				bestOpl=newOpl;
 				this.writeSolution(bestOpl);
+				System.out.println(bestOpl.getKost());
 			}
-			else
-			{
-				//System.out.println("Geen betere Opl " + bestOpl.getKost() + ", " + newOpl.getKost());
-			}
+
+			
 			if( (counter%100000)==0) {
 				counter=0;
 				System.out.println("\tBRRrrRRrrRrR CalCuLATinG......");
 			}	
+			
 		}
 		*/
 		
 		//Optie 2
+		
 		Oplossing bestOpl = initOpl.getOpl();
 		bestOpl.setKost(bestOpl.calcKost());
 		Oplossing allTimeBestOpl = bestOpl.duplicate();
 		allTimeBestOpl.koppelReq(allTimeBestOpl);
 
 		int teller = 0;
-		while(true)
+		while(System.currentTimeMillis()<end)
 		{
 			ArrayList<Oplossing> potOpl = new ArrayList<Oplossing>();
-			System.out.println("===");
+			//System.out.println("===");
 			for(int i = 0; i<bestOpl.getAutos().size(); i++)
 			{
 				Oplossing newOpl = bestOpl.duplicate();
@@ -65,16 +73,20 @@ public class Main {
 			for(int i = 0; i<bestOpl.getReq().size(); i++)
 			{
 				Oplossing newOpl = bestOpl.duplicate();
+				
 				newOpl.changeOrder(i);
+				//newOpl.changeOrder();
+				newOpl.changeOrderWithinReq(i);
 				newOpl.koppelReq(newOpl);
+				
 				potOpl.add(newOpl);				
 			}
 			for(int i = 0;i<potOpl.size(); i++)
 			{
 				if(potOpl.get(i).getKost() < bestOpl.getKost())
 				{
-					System.out.println("better solution found!");
-					System.out.println("Old Cost: "+ bestOpl.getKost()+" , New Cost : " + potOpl.get(i).getKost());
+					//System.out.println("better solution found!");
+					//System.out.println("Old Cost: "+ bestOpl.getKost()+" , New Cost : " + potOpl.get(i).getKost());
 					
 					bestOpl = potOpl.get(i);
 					teller = 0;
@@ -84,6 +96,7 @@ public class Main {
 						allTimeBestOpl = bestOpl.duplicate();
 						allTimeBestOpl.koppelReq(allTimeBestOpl);
 						this.writeSolution(allTimeBestOpl);
+						System.out.println(allTimeBestOpl.getKost());
 					}
 				}
 			}
@@ -93,11 +106,12 @@ public class Main {
 				int tmpKost = bestOpl.getKost();
 				bestOpl.changeOne((int)(Math.random() * bestOpl.getAutos().size()));
 				bestOpl.koppelReq(bestOpl);
-				System.out.println("\n random change");
-				System.out.println("Old Cost: "+ tmpKost+" , New Cost : " + bestOpl.getKost());
+				//System.out.println("\n random change");
+				//System.out.println("Old Cost: "+ tmpKost+" , New Cost : " + bestOpl.getKost());
 			}
 			teller++;
 		}
+		
 		
 	}
 	
@@ -105,7 +119,7 @@ public class Main {
 		Uitschrijver writer = new Uitschrijver(curOplossing);
 		try {
 			writer.Schrijven();
-			System.out.println("oplossing --> weggeschreven!");
+			//System.out.println("oplossing --> weggeschreven!");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
